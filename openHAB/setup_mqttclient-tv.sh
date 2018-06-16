3b4d43012bc033ca875753355df5d4b12b42083d
Simon Lee <simon@leefamily.uk.net>
2018-06-16 13:31:25 +0100

    Update setup_mqttclient-tv.sh

--- openHAB/setup_mqttclient-tv.sh
+++ openHAB/setup_mqttclient-tv.sh
@@ -36,6 +36,8 @@ mkdir /media/mqtt_setup
 cd /media
 wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient-tv/mqtt.remote_command.py
 wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient-tv/remote_services.conf
+wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient-tv/reboot_to_retropi.sh
+#wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient-tv/reboot_to_osmc.sh
 
 # Pull Service Files From GitHub to Local Device
 cd /etc/systemd/system
