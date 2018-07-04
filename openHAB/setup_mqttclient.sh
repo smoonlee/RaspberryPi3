feeae033d91bf171e8669dd340516062a58ff993
Simon Lee <simon@leefamily.uk.net>
2018-07-04 21:24:33 +0100

    Update setup_mqttclient.sh

--- openHAB/setup_mqttclient.sh
+++ openHAB/setup_mqttclient.sh
@@ -23,6 +23,7 @@ apt install git -y
 
 echo 
 apt-get install build-essential python-dev python-openssl -y
+apt-get install rpi.gpio
 
 echo 
 apt install python-pip -y
@@ -34,59 +35,27 @@ systemctl enable mosquitto
 systemctl start mosquitto
 
 echo
-mkdir /media/mqtt_setup
-
-cd /media/mqtt_setup
+mkdir /mqtt_client
+mkdir /mqtt_client 
+cd /mqtt_client/mqtt_setup
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git
 cd Adafruit_Python_DHT
 sudo python setup.py install
 
-# Pull Files from GitHub to Local Device
-cd /media
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient/media/mqtt.remote_command.py
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient/media/remote_services.conf
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient/media/dht22_output.py
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient/media/mqtt.dht22.py
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient/media/mqtt.relay0_1.py
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient/media/mqtt.relay0_2.py
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient/media/mqtt.relay0_3.py
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient/media/mqtt.relay0_4.py
-
-# Pull Files from GitHub to Local Device
-cd /media/mqtt_setup
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient/media/mqtt_setup/gpiotest.py
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient/media/mqtt_setup/mqtt.dhtsensor.py
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient/media/mqtt_setup/mqtt.gpiocontrol.py
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient/media/mqtt_setup/mqtt-launcher.py
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient/media/mqtt_setup/launcher.conf
-
-# Pull Service Files From GitHub to Local Device
-cd /etc/systemd/system
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient/etc/systemd/system/mqtt.remote_command.service
-systemctl enable mqtt.remote_command
-systemctl start mqtt.remote_command
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient/etc/systemd/system/mqtt.dht22.service
-systemctl enable mqtt.dht22
-systemctl startmqtt.dht22
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient//etc/systemd/system/mqtt.leds.service #maybe change to mqtt.lights
-systemctl enable mqtt.leds
-systemctl start mqtt.leds
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient//etc/systemd/system/mqtt.relay_1.service
-systemctl enable mqtt.relay_1
-systemctl start mqtt.relay_1
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient//etc/systemd/system/mqtt.relay_2.service
-systemctl enable mqtt.relay_2
-systemctl start mqtt.relay_2
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient//etc/systemd/system/mqtt.relay_3.service
-systemctl enable mqtt.relay_3
-systemctl start mqtt.relay_3
-wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient//etc/systemd/system/mqtt.relay_4.service
-systemctl enable mqtt.relay_4
-systemctl start mqtt.relay_4
-
 # Setup Raspotify Connector
 echo
-curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
+
+# Install curl and https apt transport
+sudo apt-get -y install curl apt-transport-https
+
+# Add repo and its GPG key
+curl -sSL https://dtcooper.github.io/raspotify/key.asc | sudo apt-key add -v -
+echo 'deb https://dtcooper.github.io/raspotify jessie main' | sudo tee /etc/apt/sources.list.d/raspotify.list
+
+# Install package
+sudo apt-get update
+sudo apt-get -y install raspotify
+
 cd /etc/default 
 rm raspotify
 wget https://raw.githubusercontent.com/smoonlee/RaspberryPi3/master/openHAB/openhab-mqttclient/etc/default/raspotify
